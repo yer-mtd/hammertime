2ad2405fa43c0ff7daf351faa1876bc71be8ae4e
MetoolDaddy <metool.daddy@yandex.ru>
2016-10-19 12:46:16 +0600

    Shenanigans

--- hammertime-utf.lua
+++ hammertime-utf.lua
@@ -1,9 +1,20 @@
+--Copyright 2016 Anton Petrochenko
+
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of GNU General Public License version 2 as published
+--by the Free Software Foundation
+
+--This program is distributed WITHOUT ANY WARRANTY; without even the implied
+--warranty of MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE.
+--See GNU General Public License for more details.
+
+--You should have recieved a copy of GNU General Public License
+--along with this program. If not, see http://www.gnu.org/licenses
+
 require "string"
-nbt = loadfile "nbt.lua"
 hammerver = 0.6
 execd = 0
 
-if nbt == nil then print "Failed to load NBT.lua" end
 
 print("MetoolDaddy's Hammertime Panel")
 print("Running v"..hammerver)
